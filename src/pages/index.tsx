178938087ed9880feb80c22db404fc6c9129d046
Elvis Felix <elvisjaspion@gmail.com>
2023-03-03 03:04:27 -0300

    add page/Home keen-slider

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,57 @@
-import { styled } from '../styles'
+import Image from 'next/image'
+import { useKeenSlider } from 'keen-slider/react'
+import { HomeContainer, Product } from '../styles/pages/home'
 
-const Button = styled('button', {
-  backgroundColor: '$rocketseat',
-  borderRadius: 8,
-  width: 90,
-  height: 35,
-  border: 'CanvasText',
-  cursor: 'pointer',
-  transition: 'all',
+import imgShirt1 from '../assets/img-Tshirt-explorer.svg'
+import imgShirt2 from '../assets/img-Tshirt-ignite.svg'
+import imgShirt3 from '../assets/img-Tshirt-maratona.svg'
 
-  '&:hover': {
-    filter: 'brightness(0.8)',
-  },
-})
+import 'keen-slider/keen-slider.min.css'
 
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48,
+    },
+  })
   return (
-    <>
-      <Button>Enviar</Button>
-    </>
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      <Product className="keen-slider__slide">
+        <Image src={imgShirt1} alt="Camisa Explorer" width={520} height={480} />
+
+        <footer>
+          <strong>Camisa Explorer</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+        <Image src={imgShirt2} alt="Camisa Explorer" width={520} height={480} />
+
+        <footer>
+          <strong>Camisa Explorer</strong>
+          <span>R$ 90,00</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+        <Image src={imgShirt3} alt="Camisa Explorer" width={520} height={480} />
+
+        <footer>
+          <strong>Camisa Explorer</strong>
+          <span>R$ 60,00</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+        <Image src={imgShirt3} alt="Camisa Explorer" width={520} height={480} />
+
+        <footer>
+          <strong>Camisa Explorer</strong>
+          <span>R$ 60,00</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   )
 }
